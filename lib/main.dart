b00ba2397afe21f76e0f5856516894eb232bbf4c
kfahmi77 <khoirulfahmi44@gmail.com>
2025-05-12 20:10:07 +0700

    add svg depedency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,26 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            children: [
+              SvgPicture.asset(
+                'assets/images/chat.svg',
+                width: 100,
+                height: 100,
+              ),
+              Text(
+                'Hello World!',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
